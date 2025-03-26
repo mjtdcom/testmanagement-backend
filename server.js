959b29b69e7ce36d5de41c0693540719639906d2
mjtdcom <132333866+mjtdcom@users.noreply.github.com>
2025-03-26 16:48:59 +0630

    server.js

--- server.js
+++ server.js
@@ -40,4 +40,6 @@ app.delete('/tests/:id', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
