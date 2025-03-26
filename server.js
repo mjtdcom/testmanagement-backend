9de457919955fe87e79b142d42c4a733ff94768f
mjtdcom <132333866+mjtdcom@users.noreply.github.com>
2025-03-26 11:01:20 +0630

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use(express.static('public'));
